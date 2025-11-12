4c645130c62abaa0677f8d8c55004adb4a20323e
matthew-hajec <matt.hajec@pm.me>
2025-11-12 05:49:08 -0500

    remove light switch component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,9 @@
 import ThemeToggle from './features/ThemeToggle'
-import LightSwitch from './components/LightSwitch'
-import { useState } from 'react'
 
 function App() {
-  const [lsOn, setLSOn] = useState(false)
-
   return (
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <ThemeToggle></ThemeToggle>
-        <LightSwitch enabled={lsOn} onToggle={() => {setLSOn(!lsOn)}}></LightSwitch>
       </div>
   )
 }
