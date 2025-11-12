ab3de87982eebd24b985d5abd6cef564c5624dff
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-12 15:10:46 -0500

    feat: implement loading state for GitHubCalendar in Stats component

--- src/sections/Stats.tsx
+++ src/sections/Stats.tsx
@@ -1,9 +1,30 @@
+import React from 'react';
 import GitHubCalendar from "../features/GitHubCalenedar";
 
 export default function Stats() {
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    // Unfortunately, GitHubCalendar doesn't provide any infomration about its loading state.
+    // Therefore, we use a simple timer and hope that the data loads within this time, not a great solution,
+    // but w
+    
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <section className="flex justify-center my-8">
-      <GitHubCalendar />
+      {isLoading ? (
+        <div className="text-gray-500 dark:text-gray-400">Loading...</div>
+      ) : (null)}
+      
+      <div style={{ display: isLoading ? 'none' : 'block' }}>
+        <GitHubCalendar />
+      </div>
     </section>
   )
 }
