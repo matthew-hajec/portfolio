72a61335ad0426688aafd66f3bc31c7da06bb81d
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-12 15:33:53 -0500

    feat: add Stack Sans Notch font to enhance typography in Name component

--- src/features/Name.tsx
+++ src/features/Name.tsx
@@ -3,7 +3,7 @@ export default function Name() {
     <div className="flex justify-center my-8">
       <h1 
         className="
-          text-5xl font-bold 
+          text-5xl font-bold font-['Stack_Sans_Notch']
           bg-linear-to-r from-blue-500 via-purple-600 to-pink-400 
           bg-clip-text text-transparent 
           animate-gradient-pan"
