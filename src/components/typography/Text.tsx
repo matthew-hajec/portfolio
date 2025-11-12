b6997a5e6851cd4ba5edcdf92173f723b7d428e9
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-12 17:44:50 -0500

    feat: update dark theme text color in Text component for improved readability

--- src/components/typography/Text.tsx
+++ src/components/typography/Text.tsx
@@ -8,5 +8,5 @@ type TextProps = {
 
 export default function Text({ children, elementType, className}: TextProps) {
   const Component = elementType || "p";
-  return <Component className={`text-gray-900 dark:text-gray-300 ${className}`}>{children}</Component>;
+  return <Component className={`text-gray-900 dark:text-gray-100 ${className}`}>{children}</Component>;
 }
