bb46d3a0f03f395f217360b3b251c8bf1bed2425
matthew-hajec <matt.hajec@pm.me>
2025-11-12 06:56:22 -0500

    add Name component to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import ThemeToggle from './features/ThemeToggle'
+import Name from './features/Name'
 
 function App() {
   return (
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <ThemeToggle></ThemeToggle>
+        <Name></Name>
       </div>
   )
 }
