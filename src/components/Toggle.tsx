048ab87787268c6990d929db49abae54bf482f93
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-12 17:28:50 -0500

    feat: update Toggle component to use Text for consistent typography

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,3 +1,5 @@
+import Text from "./typography/Text"
+
 type ToggleProps = {
   isChecked: boolean
   onChange: (checked: boolean) => void
@@ -9,8 +11,8 @@ export default function Toggle({isChecked, onChange, children, selectable = true
   return (
     <label className={`inline-flex items-center cursor-pointer ${selectable ? '' : 'select-none'}`}>
       <input type="checkbox" checked={isChecked} onChange={e => onChange(e.target.checked)} className="sr-only peer"></input>
-      <div className="relative w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-      <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{children}</span>
+      <div className="relative w-11 h-6 bg-white rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-gray-200 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+      <Text elementType="span" className="ms-3 text-sm font-bold">{children}</Text>
     </label>    
   )
 }
