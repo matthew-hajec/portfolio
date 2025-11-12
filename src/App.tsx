efc30d42b2568b0fec1e650c79b44902dcd4a026
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-12 15:25:57 -0500

    feat: restructure App component layout to include Sidebar and improve main content organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import ThemeToggle from './features/ThemeToggle'
 import Name from './features/Name'
 import Stats from './sections/Stats'
+import Sidebar from './features/Sidebar'
 
 function App() {
   return (
-      <div className="min-h-screen bg-white dark:bg-gray-900">
-        <ThemeToggle></ThemeToggle>
-        <Name></Name>
+      <div className="min-h-screen bg-white dark:bg-gray-900 flex">
+        <Sidebar></Sidebar>
 
-        <main className="mx-auto px-4 mt-8">
+        <main className="px-4 mt-8 flex-1">
+          <div className="flex justify-end">
+            <ThemeToggle />
+          </div>
+          <Name></Name>
+
           <Stats></Stats>
         </main>
 
