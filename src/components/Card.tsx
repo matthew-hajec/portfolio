b5d7eaa170255d0b32e7d9fc97907f2ec23fa059
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 13:13:23 -0500

    fix: add additionalClasses prop to Card component for better customization

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,23 +2,27 @@ type CardProps = {
   children: React.ReactNode
   hoverEffect?: boolean
   paddingSize?: number
+  additionalClasses?: string
 
 } & React.HTMLAttributes<HTMLDivElement>;
 
-export default function Card({ children, hoverEffect, paddingSize=8, ...props }: CardProps) {
+export default function Card({ children, hoverEffect, paddingSize=8, additionalClasses='', ...props }: CardProps) {
   return (
     <div className={`
       w-full
       p-${paddingSize}
       ${hoverEffect ? `
-        hover:bg-white/25 transition-all ease-in-out duration-500
-        hover:shadow-white/30
+        hover:bg-white/25 hover:shadow-white/30
+        dark:hover:bg-gray-800/60 dark:hover:shadow-black/30
+
+        transition-all ease-in-out duration-500
         ` : ''}
       rounded-xl shadow-lg
       shadow-white/12 dark:shadow-black/20
       border border-white/20 dark:border-gray-700
       bg-white/20 dark:bg-gray-800/40
       backdrop-blur-xl
+      ${additionalClasses}
     `} {...props}>
       {children}
     </div>
