2b53e14387a59ae2ddc40d61c92d5375f88abd1d
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 12:00:40 -0500

    add glass effect to card

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,22 @@ import Introduction from './sections/Introduction'
 
 function App() {
   return (
-      <div className="min-h-screen bg-gray-200 dark:bg-gray-900">
+      <div className="
+        relative 
+        min-h-screen
+        bg-[linear-gradient(72deg,rgba(255,255,255,1)_0%,rgba(148,191,255,1)_30%,rgba(129,187,120,1)_54%,rgba(217,174,100,1)_83%,rgba(255,255,255,1)_100%)]
+                dark:bg-[linear-gradient(72deg,rgba(17,24,39,1)_0%,rgba(75,85,101,1)_30%,rgba(79,70,229,1)_100%)]
+      ">
+        {/* Background Circles */}
+        <div className='
+          fixed
+          inset-0
+          overflow-hidden
+        '>
+          <div className="absolute bottom-0 -right-3/4 h-3/1 aspect-square -rotate-45  rounded-full bg-orange-600/10 blur-xl"></div>
+          <div className="absolute -bottom-1/2 left-1/4 h-3/4 w-3/4 rounded-full bg-pink-600/15 blur-3xl"></div>
+        </div>
+
         <div className="max-w-7xl mx-auto pt-12">
           <header className='relative'>
             <Name />
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,13 @@
 export default function Card({ children }: { children: React.ReactNode }) {
   return (
     <div className="
-      bg-white
-      dark:bg-gray-800 
-      rounded-lg
-      shadow-xl
-      p-6
+      w-full
+      p-8
+      rounded-xl shadow-lg
+      shadow-white/12 dark:shadow-black/20
+      border border-white/20 dark:border-gray-700
+      bg-white/20 dark:bg-gray-800/40
+      backdrop-blur-xl
     ">
       {children}
     </div>
