a30919154d5ccf3b08803cc10e37a5489e034d2f
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 15:43:21 -0500

    style: enhance visual effects in Sidebar and App components

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         relative 
         min-h-screen
         bg-[linear-gradient(72deg,rgba(255,255,255,1)_0%,rgba(148,191,255,1)_30%,rgba(129,187,120,1)_54%,rgba(217,174,100,1)_83%,rgba(255,255,255,1)_100%)]
-        dark:bg-[linear-gradient(72deg,rgba(17,24,39,1)_0%,rgba(65,85,101,1)_30%,rgba(79,70,229,1)_100%)]
+        dark:bg-[linear-gradient(72deg,rgba(17,24,39,1)_0%,rgba(65,85,101,1)_30%,rgba(79,70,219,1)_100%)]
       ">
         {/* Background Circles */}
         <div className='
@@ -19,8 +19,13 @@ function App() {
           inset-0
           overflow-hidden
         '>
-          <div className="absolute bottom-0 -right-3/4 h-3/1 aspect-square -rotate-45  rounded-full bg-orange-600/10 blur-xl"></div>
+          <div className="absolute bottom-0 -right-3/4 h-3/1 aspect-square -rotate-45  rounded-full bg-orange-600/15 blur-xl"></div>
           <div className="absolute -bottom-1/2 left-1/4 h-3/4 w-3/4 rounded-full bg-pink-600/15 blur-3xl"></div>
+          <div className="absolute top-0 -left-1/5 h-2/3 w-2/3 rounded-full bg-green-600/5 blur-2xl"></div>
+          <div className="absolute top-1/2 end-0 h-1/2 w-1/2 rounded-full bg-blue-600/10 blur-2xl"></div>
+          <div className="absolute bottom-0 left-1/4 h-1/3 w-1/3 rounded-full bg-purple-600/10 blur-2xl"></div>
+          <div className="absolute top-1/3 left-0 h-1/2 w-3/4 rounded-full bg-yellow-600/10 blur-2xl"></div>
+          <div className="absolute top-0 right-0 h-2/3 w-2/3 rounded-full bg-blue-600/5 blur-2xl"></div>
         </div>
 
         <div className="max-w-7xl mx-auto pt-12 pb-6 px-6">
--- src/features/Sidebar.tsx
+++ src/features/Sidebar.tsx
@@ -13,7 +13,10 @@ function LinkItem({ href, children }: { href: string; children: React.ReactNode 
           border dark:border-gray-100/20 border-gray-900/20
           cursor-pointer
           hover:bg-gray-100/50 dark:hover:bg-gray-800/50
+          hover:scale-101
           transition-all ease-in-out duration-200
+          shadow-md shadow-gray-500/10
+
         ">
           <Text elementType="p">
             {children}
