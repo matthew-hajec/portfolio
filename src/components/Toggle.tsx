a655958ea27faf5f7e1dac2af238dfb0a38c4ffd
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 12:19:56 -0500

    feat: enhance Toggle component styling with improved dimensions and shadow effects

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -11,7 +11,25 @@ export default function Toggle({isChecked, onChange, children, selectable = true
   return (
     <label className={`inline-flex items-center cursor-pointer ${selectable ? '' : 'select-none'}`}>
       <input type="checkbox" checked={isChecked} onChange={e => onChange(e.target.checked)} className="sr-only peer"></input>
-      <div className="relative w-11 h-6 bg-white rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-0.5 after:bg-gray-200 after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
+      <div className="
+        relative w-13 h-7
+        bg-white/50 dark:bg-gray-700/50
+        dark:border-gray-600
+        shadow-lg
+        shadow-white/12 dark:shadow-black/20
+        rounded-full 
+
+        peer 
+        peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800
+        peer-checked:bg-blue-600/50
+        peer-checked:after:start-7 rtl:peer-checked:after:-translate-x-full after:content-[''] 
+        
+
+        after:absolute
+        after:bg-white/80 dawgdark:after:bg-white/50
+        after:top-1 after:start-1 after:rounded-full after:h-5 after:w-5 after:transition-all 
+        
+       "></div>
       <Text elementType="span" className="ms-3 text-sm font-bold">{children}</Text>
     </label>    
   )
