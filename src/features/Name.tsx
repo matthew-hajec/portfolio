c83a31d930d3a76c4a5434f4670faac0d3ce1d72
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 12:48:29 -0500

    fix: update gradient colors for improved visibility in Name component

--- src/features/Name.tsx
+++ src/features/Name.tsx
@@ -4,7 +4,8 @@ export default function Name() {
       <h1 
         className="
           text-6xl font-bold font-['Stack_Sans_Notch']
-          bg-linear-to-r from-blue-500 via-purple-600 to-pink-400 
+          bg-linear-to-r from-blue-700 via-purple-600 to-pink-500 
+          dark:from-blue-500 dark:via-purple-600 dark:to-pink-400
           bg-clip-text text-transparent 
           animate-gradient-pan"
         style={{ backgroundSize: '200% 100%' }}
