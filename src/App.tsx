546d414c4a8a71b3b80ff9ac5adf78d904bf949c
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 22:24:41 -0500

    fix: add bg-fixed class for improved background behavior

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ function App() {
       <div className="
         relative 
         min-h-screen
+        bg-fixed
         bg-[linear-gradient(72deg,rgba(255,255,255,1)_0%,rgba(148,191,255,1)_30%,rgba(129,187,120,1)_54%,rgba(217,174,100,1)_83%,rgba(255,255,255,1)_100%)]
         dark:bg-[linear-gradient(72deg,rgba(17,24,39,1)_0%,rgba(65,85,101,1)_30%,rgba(79,70,219,1)_100%)]
       ">
