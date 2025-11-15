7f2cefbc58d0962234ef6f746edff1683dbf4ba3
matthew-hajec <matthew.hajec02@gmail.com>
2025-11-14 22:38:32 -0500

    update layout of Education section

--- src/sections/Education.tsx
+++ src/sections/Education.tsx
@@ -10,15 +10,18 @@ export default function Education() {
         Education
       </Text>
       <Card padded={false} additionalClasses="p-4">
-      <div className="flex items-center flex-col justify-around md:flex-row gap-8">
-        <Icon iconName="mga" className="w-75" />
-        <Text className="text-md">
-            <span className="font-bold">Macon, GA</span><br />
-            <span className="font-semibold">Bachelor of Science in Information Technology</span><br />
-
-            Graduating December 2025
-        </Text>
-      </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+          <div className="flex justify-center items-center">
+            <Icon iconName="mga" className="w-75" />
+          </div>
+          <div className="flex justify-start items-center">
+            <Text className="text-md">
+              <span className="font-bold">Macon, GA</span><br />
+              <span className="font-semibold">Bachelor of Science in Information Technology</span><br />
+              Graduating December 2025
+            </Text>
+          </div>
+        </div>
       </Card>
     </Card>
     </>
